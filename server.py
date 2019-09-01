9b876ce92398400819724eaa8c5815ea860bdd82
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-09-01 19:09:33 +0530

    Updated server

--- server.py
+++ server.py
@@ -2,16 +2,8 @@ from pyrebase import pyrebase
 from flask import Flask, request, jsonify
 import pandas as pd
 import pickle
-config = {
-    "apiKey": "AIzaSyCfEjJpkUdFooIkdqdRj_TZng1imrmymMY",
-    "authDomain": "my-portfolio-248515.firebaseapp.com",
-    "databaseURL": "https://my-portfolio-248515.firebaseio.com",
-    "projectId": "my-portfolio-248515",
-    "storageBucket": "my-portfolio-248515.appspot.com",
-    "messagingSenderId": "970529353485",
-    "appId": "1:970529353485:web:529efcd5de1ede64"
-}
-firebase = pyrebase.initialize_app(config)
+# config  = YOUR AZURE CONFIG HERE
+base = pyrebase.initialize_app(config)
 db = firebase.database()
 
 app = Flask(__name__)
@@ -67,17 +59,17 @@ def formInput():
     y = model.predict_proba(val2)
     y = y[0][0]
     data = {
-        "age":val[0],
-        "family_history":val[1],
-        "leave":val[2],
-        "work_interfere":val[3],
-        "benefits":val[4],
-        "anonymity":val[5],
-        "phys_health_consequence":val[6],
-        "mental_health_interview":val[7],
-        "obs_consequence":val[8],
-        "prediction":y,
-        "phone":int(x['phone'])
+        "age": val[0],
+        "family_history": val[1],
+        "leave": val[2],
+        "work_interfere": val[3],
+        "benefits": val[4],
+        "anonymity": val[5],
+        "phys_health_consequence": val[6],
+        "mental_health_interview": val[7],
+        "obs_consequence": val[8],
+        "prediction": y,
+        "phone": int(x['phone'])
     }
     db.child("monthly_data").push(data)
 
